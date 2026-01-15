1b2468ccc2a373fea88a7498b113f4811ce4612e
Tunczykmarcel <marceltanczyk162@gmail.com>
2026-01-15 13:57:40 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,12 @@
 package org.example;
 
-public class Main{
+public class Main {
 
-    public static int add(int a,int b){
-        return a+b;
+    public static int add(int a, int b) {
+        return a + b;
     }
 
-    public static void main(String[] args){
-        System.out.println(add(2,2));
-    } 
+    public static void main(String[] args) {
+        System.out.println(add(2, 2));
+    }
 }
